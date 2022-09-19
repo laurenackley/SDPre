cbf131afd116d32e12e60445cee321f89c6b35af
Lauren Ackley <lackley127@gmail.com>
2022-09-19 14:00:38 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String [] args) {
+    String name = "Lauren Ackley";
+    int age = 26;
+    String state = "Texas";
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+  }
+}
